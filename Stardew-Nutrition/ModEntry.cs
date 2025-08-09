47c40db2ef429423a53a39fece8081a853bbbf03
GXatBuffalo <gavinxie@buffalo.edu>
2025-08-08 20:52:40 -0400

    Set up some base classes and data structures needed in the future.

--- Stardew-Nutrition/ModEntry.cs
+++ Stardew-Nutrition/ModEntry.cs
@@ -5,36 +5,102 @@ using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
 
-namespace YourProjectName
+namespace StardewNutrition
 {
-	/// <summary>The mod entry point.</summary>
-	internal sealed class ModEntry : Mod
+	public class SoilNutrition
 	{
-		/*********
-		** Public methods
-		*********/
-		/// <summary>The mod entry point, called after the mod is first loaded.</summary>
-		/// <param name="helper">Provides simplified APIs for writing mods.</param>
-		public override void Entry(IModHelper helper)
+		public int nitro { get; set; }
+		public int phos { get; set; }
+		public int pH { get; set; }
+		public int iridium { get; set; }
+
+		public SoilNutrition(int a, int b, int c, int d, int e)
 		{
-			helper.Events.Input.ButtonPressed += this.OnButtonPressed;
-		}
-
-
-		/*********
-		** Private methods
-		*********/
-		/// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-		/// <param name="sender">The event sender.</param>
-		/// <param name="e">The event data.</param>
-		private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
-		{
-			// ignore if player hasn't loaded a save yet
-			if (!Context.IsWorldReady)
-				return;
-
-			// print button presses to the console window
-			this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+			nitro = a;
+			phos = b;
+			pH = c;
+			iridium = d;
 		}
 	}
+
+	public class NutritionMap {
+		public int mapMana { get; set; }
+		public SoilNutrition[,] mapData { get; set; }
+		public Dictionary<ValueTuple,int> magicCrops { get; set; }
+
+		public NutritionMap (int sizeX, int sizeY, int mana){
+			mapMana = mana;
+			mapData = new SoilNutrition [sizeX, sizeY];
+			magicCrops = new Dictionary<ValueTuple,int> ();
+		}
+	}
+	public class nutritionHandler {
+		public int nutriMin = 0;
+		public int nutriMax = 100;
+		public void SetNitro(SoilNutrition dirtData, int n) 
+		{
+			if (n > nutriMax)
+			{
+				dirtData.nitro = nutriMax;
+			}
+			else if (n < nutriMin)
+			{
+				dirtData.nitro = nutriMin;
+			}
+			else
+			{
+				dirtData.nitro = n;
+			}
+		}
+		public void SetPhos(SoilNutrition dirtData, int p)
+		{
+			if (p > nutriMax)
+			{
+				dirtData.phos = nutriMax;
+			}
+			else if (p < nutriMin)
+			{
+				dirtData.phos = nutriMin;
+			}
+			else
+			{
+				dirtData.phos = p;
+			}
+		}
+		public void SetpH(SoilNutrition dirtData, int x)
+		{
+			if (x > nutriMax)
+			{
+				dirtData.pH = nutriMax;
+			}
+			else if (x < nutriMin)
+			{
+				dirtData.pH = nutriMin;
+			}
+			else
+			{
+				dirtData.pH = x;
+			}
+		}
+
+		public void SetIridium(SoilNutrition dirtData, int i)
+		{
+			if (i > nutriMax)
+			{
+				dirtData.iridium = nutriMax;
+			}
+			else if (i < nutriMin)
+			{
+				dirtData.iridium = nutriMin;
+			}
+			else
+			{
+				dirtData.iridium = i;
+			}
+		}
+
+
+
+	}
+
 }
