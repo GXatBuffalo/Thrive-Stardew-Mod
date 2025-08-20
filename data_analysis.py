f5081c86c8fbc44eb23f01faa591c15148dc2d2b
GXatBuffalo <gavinxie@buffalo.edu>
2025-08-20 13:16:49 -0400

    adds new graphs and labels all charts in data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -19,16 +19,32 @@ print(requirements_list)
 print(depreciation_list)
 # suppose data["values"] = [numbers...]
 requirements_list_np = np.array(requirements_list)
+depreciation_list_np = np.array(depreciation_list)
 
-print(requirements_list_np)
 values = ["nitro", "phos", "pH", "iridium", "mana"]
 
 for x in range(0, 5):
     print("Analysis of: ", values[x])
-    print("Mean:", np.mean(requirements_list_np[:, x]))
-    print("Median:", np.median(requirements_list_np[:, x]))
-    print("Max:", np.max(requirements_list_np[:, x]))
-    print("Min:", np.min(requirements_list_np[:, x]))
+    print("Mean:", np.mean(requirements_list_np[:, x*2]))
+    print("Median:", np.median(requirements_list_np[:, x*2]))
+    print("Max:", np.max(requirements_list_np[:, x*2]))
+    print("Min:", np.min(requirements_list_np[:, x*2]))
     # plot
-    plt.hist(requirements_list_np[:, x], bins=1)
+    plt.title("Requirements: " + values[x])
+    plt.ylabel("Y-axis: Range")
+    plt.xlabel("X-axis: Count")
+    plt.hist(requirements_list_np[:, x*2], bins=200)
+    plt.show()
+
+for x in range(0, 5):
+    print("Analysis of: ", values[x])
+    print("Mean:", np.mean(depreciation_list_np[:, x]))
+    print("Median:", np.median(depreciation_list_np[:, x]))
+    print("Max:", np.max(depreciation_list_np[:, x]))
+    print("Min:", np.min(depreciation_list_np[:, x]))
+
+    plt.title("Soil Depreciation: " + values[x])
+    plt.ylabel("Y-axis: Range")
+    plt.xlabel("X-axis: Count")
+    plt.hist(depreciation_list_np[:, x], bins=200)
     plt.show()
