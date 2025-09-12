584f434b0392d5d24c2567bc0eac56f7e9b4d533
GXatBuffalo <gavinxie@buffalo.edu>
2025-09-11 23:48:03 -0400

    Updated SoilPropertiesMap attributes. Added min and max coords, used to do less checks.

--- src/Domain/SoilPropertiesMap.cs
+++ src/Domain/SoilPropertiesMap.cs
@@ -6,17 +6,28 @@ namespace Thrive.src.Domain
 	public class SoilPropertiesMap
 	{
 		public int DataVersion { get; set; }
-		public int ManaMax { get; set; }
-		public int MapMana { get; set; }
-		public SoilProperties[][] MapData { get; set; }
-		public Dictionary<(int, int), int> MagicCrops { get; set; } = new();
 
+		public int MapMana { get; set; }  // amount of Mana on the map. 
+		public int ManaMax { get; set; }  // limit on Mana on the map.
+
+		public SoilProperties[,] MapData { get; set; }
+		public int minY { get; set; }			// lowest tile on Y axis farmer has hoed;
+		public int maxY { get; set; }     // highest tile on Y axis farmer has hoed;
+		public int minX { get; set; }     // lowest tile on X axis farmer has hoed;
+		public int maxX	{  get; set; }    // highest tile on X axis farmer has hoed;
+		
+		// dictionary to hold location of magic crops using their coords, value is mana drain
+		public Dictionary<(int, int), int> MagicCrops { get; set; } = new();		
+
+		// Constructor. Parameters are map size and mana map starts with.
 		public SoilPropertiesMap(int sizeX, int sizeY, int initialMana)
 		{
 			MapMana = initialMana;
-			MapData = new SoilProperties[sizeY][];
-			for (int i = 0; i < sizeY; i++)
-				MapData[i] = new SoilProperties[sizeX];
+			MapData = new SoilProperties[sizeY, sizeX];
+			minX = 0;
+			maxX = sizeX-1;
+			minY = 0; 
+			maxY = sizeY-1;
 		}
 
 		public void AddMagicCrop(int x, int y, int manaCost) => MagicCrops[(x, y)] = manaCost;
