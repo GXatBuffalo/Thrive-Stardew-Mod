c425cc272c8b77bc81174b15d17128441a5ddea3
GXatBuffalo <gavinxie@buffalo.edu>
2025-09-18 20:13:25 -0400

    Another attempt at fixing Harmony patch. Usage was misunderstood in earlier attempts.

--- src/Services/HarmonyHarvest.cs
+++ src/Services/HarmonyHarvest.cs
@@ -1,7 +1,9 @@
 ﻿using HarmonyLib;
 using StardewModdingAPI;
+using StardewValley;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 
 namespace Thrive.src.Services
 {
@@ -40,17 +42,35 @@ namespace Thrive.src.Services
 			CodeMatcher matcher = new(instructions);
 			MethodInfo myCropQualityInfo = AccessTools.Method(typeof(FarmingHandler), nameof(FarmingHandler.OnHarvest_GetCropQuality));
 			MethodInfo addPropertiesInfo = AccessTools.Method(typeof(CropQuality_HarmonyPatch), nameof(AddProperties));
+			MethodInfo getMapNameInfo = AccessTools.PropertyGetter(typeof(StardewValley.GameLocation), nameof(StardewValley.GameLocation.Name));
 
+			// match first loading of variable 'cropQuality'
+			matcher.MatchStartForward(new CodeMatch(OpCodes.Stloc_S, (byte)14))
+						 .ThrowIfNotMatch($"Could not find entry point for {nameof(HarvestCrop_Transpiler)}")
+						 .Advance(-1);
+			int startIndex = matcher.Pos;
 
-			matcher.MatchStartForward(
-				new CodeMatch(OpCodes.Stloc, 14)
-				)
-				.ThrowIfNotMatch($"Could not find entry point for {nameof(HarvestCrop_Transpiler)}")
-				.RemoveInstructionsWithOffsets(-1, 49)
-				.Insert(
-					new CodeInstruction(OpCodes.Call, myCropQualityInfo),
-					new CodeInstruction(OpCodes.Call, addPropertiesInfo)
-				);
+			matcher.MatchStartForward(new CodeMatch(OpCodes.Stloc_S, (byte)14))
+			 .MatchStartForward(new CodeMatch(OpCodes.Stloc_S, (byte)14))
+			 .MatchStartForward(new CodeMatch(OpCodes.Stloc_S, (byte)14))
+			 .MatchStartForward(new CodeMatch(OpCodes.Stloc_S, (byte)14))
+			 .ThrowIfNotMatch($"Could not find end point match for {nameof(HarvestCrop_Transpiler)}");
+
+			int endIndex = matcher.Pos;
+
+			matcher.RemoveInstructionsInRange(startIndex, endIndex + 1)
+						 .InsertAndAdvance
+						 (
+								new CodeInstruction(OpCodes.Ldloc_1),
+								new CodeInstruction(OpCodes.Ldarg_0),
+								new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(StardewValley.Crop), "currentLocation")),
+								new CodeInstruction(OpCodes.Callvirt, getMapNameInfo),
+								new CodeInstruction(OpCodes.Ldarg_1),
+								new CodeInstruction(OpCodes.Ldarg_2),
+								new CodeInstruction(OpCodes.Call, myCropQualityInfo),
+								new CodeInstruction(OpCodes.Stloc_S, 14),
+								new CodeInstruction(OpCodes.Call, addPropertiesInfo)
+							);
 
 			return matcher.InstructionEnumeration();
 		}
